607343d08d1f922a35309924c49e262966a95753
Gnanesh G <gnanesh9206@gmail.com>
2025-12-28 15:35:33 +0530

    ml prediction logic updated

---
+++ model/predict.py
@@ -0,0 +1,14 @@
+import pickle
+import pandas as pd
+
+
+
+MODEL_VERSION = '1.0.0'
+
+with open('model\model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+def predict_output(user_input:dict):
+    input_df = pd.DataFrame([user_input])
+    output = model.predict(input_df)[0]
+    return output
