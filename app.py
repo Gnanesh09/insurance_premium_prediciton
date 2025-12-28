2191e5fcc3b6b90b2626abcf4d2768a746fcc079
Gnanesh G <gnanesh9206@gmail.com>
2025-12-28 15:09:03 +0530

    user input schema added and tiny fixes in app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field,computed_field
-from typing import Literal,Annotated
+
 import pickle
 import pandas as pd
 
 from fastapi.responses import JSONResponse
+
+from schema.user_input import UserInput
 
 MODEL_VERSION = '1.0.0'
 
@@ -22,52 +23,7 @@ with open('model\model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = FastAPI()
-class UserInput(BaseModel):
-    age: Annotated[int , Field(...,gt=0, lt=120,description="Age of the user")]
-    weight:Annotated[float , Field(...,gt=0, description="weight of the user")]
-    height:Annotated[float , Field(...,gt=0, lt=2.5,description="height of the user")]
-    income_lpa:Annotated[float , Field(...,gt=0, description="salry of the user")]
-    smoker:Annotated[bool , Field(...,description="is user smoker")]
-    city:Annotated[str , Field(..., description="city of the user")]
-    occupation: Annotated[Literal['Student', 'Business', 'Govt Employee', 'Engineer', 'Teacher',
-       'Doctor'] , Field(..., description="occupation of the user")]
 
-    @computed_field
-    @property
-    def bmi(self)->float:
-        return self.weight/(self.height**2)
-
-    @computed_field
-    @property
-    def lifestyle_risk(self)->str:
-
-        if self.smoker and self.bmi > 30:
-            return "high"
-        elif self.smoker or self.bmi > 27:
-            return "medium"
-        else:
-            return "low"
-
-    @computed_field
-    @property
-    def age_group(self)->str:
-        if self.age < 25:
-            return "young"
-        elif self.age < 45:
-            return "adult"
-        elif self.age < 60:
-            return "middle_aged"
-        return "senior"
-
-    @computed_field
-    @property
-    def city_tier(self)->int:
-        if self.city in tier_1_cities:
-            return 1
-        elif self.city in tier_2_cities:
-            return 2
-        else:
-            return 3
 
 
 @app.get("/")
